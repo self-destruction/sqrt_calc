49800b9041dd833f481f0c5346e64d5409971af1
charj <dev@charlie.fyi>
2018-03-16 05:14:15 +0000

    Moving form logic

---
+++ src/Form/Calculator/DisplayType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form\Calculator;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DisplayType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('equation', TextType::class, [
+                'attr' => [
+                    'autofocus' => "autofocus",
+                    ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => '='
+            ]);
+    }
+}
